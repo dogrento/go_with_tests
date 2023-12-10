2698e3877608128c527b2dc2575ae1abb4d366df
dogrento <dogrento93@gmail.com>
2023-12-10 05:32:16 -0300

    constants intro

--- hello.go
+++ hello.go
@@ -3,9 +3,11 @@ package main
 // Importing packages
 import "fmt"
 
+const helloPrefix = "Hello, "
+
 //Creating a function that returns a string
 func Hello(name string) string {
-  return "Hello " + name
+  return helloPrefix + name
 }
 
 func main(){
