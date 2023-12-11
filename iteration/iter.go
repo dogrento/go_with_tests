11d50e517650fd064bdb89276ec0e22eeaccca2b
dogrento <dogrento93@gmail.com>
2023-12-10 22:56:48 -0300

    benchmark test

---
+++ iteration/iter.go
@@ -0,0 +1,12 @@
+package iteration
+
+func Repeat(chars string) string{
+  // declaring variable typed as string
+  var rep string
+  
+  for i := 0; i < 5; i++{
+    //  following line is the same as rep = rep + chars (Add AND)
+    rep += chars
+  }
+  return rep 
+}
