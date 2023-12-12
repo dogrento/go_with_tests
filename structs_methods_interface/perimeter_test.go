6e194c978317865263e50b3310c99a3bf723589c
dogrento <dogrento93@gmail.com>
2023-12-11 21:13:29 -0300

    creating area test

--- structs_methods_interface/perimeter_test.go
+++ structs_methods_interface/perimeter_test.go
@@ -12,8 +12,26 @@ func TestPerimeter(t *testing.T){
     got := Perimeter(width, height)
     want := 40.0 
 
-    if got != want{
-      t.Errorf("\ngot -> %.2f\nwant -> %.2f", got, want)
-    }
+    // if got != want{
+    //   t.Errorf("\ngot -> %.2f\nwant -> %.2f", got, want)
+    // }
+    CompareResult(t, got, want)
   })
+}
+
+func TestArea(t *testing.T){
+  t.Run("Area Result", func(t *testing.T){
+    got := Area(12.0, 6.0)
+    want := 72.0
+
+    CompareResult(t, got, want)
+  })
+}
+
+func CompareResult(t testing.TB, got float64, want float64){
+  t.Helper() 
+
+  if got != want{
+    t.Errorf("\ngot -> %.2f\nwant -> %.2f", got, want)
+  }
 }
