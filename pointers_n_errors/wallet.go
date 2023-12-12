46039943b0bd41b3a888c87ef2826310a502d526
dogrento <dogrento93@gmail.com>
2023-12-12 04:26:11 -0300

    creating new type bitcoin from float

--- pointers_n_errors/wallet.go
+++ pointers_n_errors/wallet.go
@@ -1,8 +1,11 @@
 package main
 
+// Creating new types from existing ones
+type Bitcoin float64
+
 type Wallet struct{
   // if a symbol starts with a lowercase then it's private outside the package
-  balance float64
+  balance Bitcoin
 }
 // The reason to use pointer is bc the var is a copy when it is passed as param
 // 'struct pointer' are automatically dereferenced
