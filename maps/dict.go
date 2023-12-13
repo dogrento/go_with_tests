7e337b7f89937787b93b539e2cdcb5695048ed8d
dogrento <dogrento93@gmail.com>
2023-12-13 03:33:23 -0300

    creating test for unknown key

--- maps/dict.go
+++ maps/dict.go
@@ -1,5 +1,6 @@
 package main
 
+// creating a wrapper Dictionary around map 
 type Dictionary map[string]string
 
 func (d Dictionary) Search(key string) string{
--- maps/dict_test.go
+++ maps/dict_test.go
@@ -5,18 +5,26 @@ import(
 )
 
 func TestSearch(t *testing.T){
-  t.Run("searching key getting it's value", func(t *testing.T){
-    // declaring map requires two types
-    // - first is the key type which is written inside the []
-    // - second is the value type, goes after []
-    // NOTE: key type is a "comparable type"
-    // dict := map[string]string{"test": "this is just a test"}
-    dict := Dictionary{"test": "this is just a test"}
+  // declaring map requires two types
+  // - first is the key type which is written inside the []
+  // - second is the value type, goes after []
+  // NOTE: key type is a "comparable type"
+  // dict := map[string]string{"test": "this is just a test"}
+  dict := Dictionary{"test": "this is just a test"}
 
+  t.Run("Known key", func(t *testing.T){
     got := dict.Search("test")
     want := "this is just a test"
 
     assertStrings(t, got, want)
+  })
+   t.Run("Unknown key", func(t *testing.T){
+    _, err := dict.Search("unknown")
+    want := "could not find the word you were looking for"
+
+    if err == nil{
+      t.Fatal("expected to get an error.")
+    }
   })
 }
 
