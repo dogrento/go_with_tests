778e6beb3aa71815af438de06dc04a3f46a6f08f
dogrento <dogrento93@gmail.com>
2023-12-14 21:23:29 -0300

    creating coutdown test

---
+++ mocking/mocking.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+  "time"
+  "fmt"
+)
+
+func Countdown(){
+  fmt.Println("qual fita porra")
+  time.Sleep(5)
+  fmt.Println("porra caralho")
+}
+
+func main(){
+  Countdown()
+}
