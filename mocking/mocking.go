da5bab2c2805bc587c9e2e092c94955a48c58165
dogrento <dogrento93@gmail.com>
2023-12-15 09:34:37 -0300

    iterate countdown

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -1,14 +1,28 @@
 package main
 
-import(
-  "fmt"
-  "bytes"
+import (
+	// "bytes"
+	"fmt"
+	"io"
+	"os"
 )
 
-func Countdown(out *bytes.Buffer){
-  fmt.Fprint(out, "3")
+const(
+  finalWord = "Go!"
+  countdownStart = 3
+)
+
+
+// it takes an io.Writer
+// and sends a string
+// func Countdown(out *bytes.Buffer){
+func Countdown(out io.Writer){
+  for i := countdownStart; i > 0; i--{
+    fmt.Fprintln(out, i)
+  }
+  fmt.Fprint(out, finalWord)
 }
 
 func main(){
-  // Countdown()
+  Countdown(os.Stdout)
 }
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -11,7 +11,11 @@ func TestCountdown(t *testing.T){
   Countdown(buffer)
 
   got := buffer.String()
-  want := "3"
+  // backtick is another way of creating a string but lets you include things like newlines
+  want := `3
+2
+1
+Go!`
 
   if got != want{
     t.Errorf("\ngot->%s\nwant->%s", got, want)
