d78c8ff257734634598d597b02095ef18cc96690
dogrento <dogrento93@gmail.com>
2023-12-16 09:11:29 -0300

    creating channel and making test pass

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,24 +1,35 @@
 package main
 
 import(
-  "time"
+  // "time"
 )
 
 type WebsiteChecker func(string) bool
+type result struct{
+  string
+  bool
+}
 
 // It returns a map of each URL checked to a bool value
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool{
   results := make(map[string]bool)
+  resultChannel := make(chan result)
 
   for _, url := range urls{
     // creating a go routine as an anonymous func
     // u -> is a copy of url so it cant be changed
     go func(u string){
-      results[u] = wc(u)
+      // results[u] = wc(u)
+      resultChannel <- result{u, wc(u)}
     }(url)
   }
 
-  time.Sleep(2 * time.Second)
+  for i := 0; i < len(urls); i++{
+    r := <-resultChannel
+    results[r.string] = r.bool
+  }
+
+  // time.Sleep(2 * time.Second)
 
   return results
 }
