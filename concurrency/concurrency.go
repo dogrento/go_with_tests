ddb4f829b832f2c22d6dd4e103d66e398b4ab692
dogrento <dogrento93@gmail.com>
2023-12-16 08:09:31 -0300

    fixing anon func to pass the test

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,5 +1,9 @@
 package main
 
+import(
+  "time"
+)
+
 type WebsiteChecker func(string) bool
 
 // It returns a map of each URL checked to a bool value
@@ -8,10 +12,13 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool{
 
   for _, url := range urls{
     // creating a go routine as an anonymous func
-    go func(){
-      results[url] = wc(url)
-    }()
+    // u -> is a copy of url so it cant be changed
+    go func(u string){
+      results[u] = wc(u)
+    }(url)
   }
+
+  time.Sleep(2 * time.Second)
 
   return results
 }
