8e580d804834bba519fbc9a82976b06622939595
dogrento <dogrento93@gmail.com>
2023-12-17 14:54:30 -0300

    creating ping()

--- select/select.go
+++ select/select.go
@@ -16,6 +16,15 @@ func Racer(a, b string) string{
   return b
 }
 
+func ping(url string) chan struct{} {
+  ch := make(chan struct{})
+  go func(){
+    http.Get(url)
+    close(ch)
+  }()
+  return ch
+}
+
 func measureResponseTime(url string) time.Duration{
   start := time.Now()
   http.Get(url)
