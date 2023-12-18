bbee7345d4f82d217a771f480c3fd620536fbe8f
dogrento <dogrento93@gmail.com>
2023-12-18 20:06:41 -0300

    refactoring to table test

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,7 +3,11 @@ package main
 import "reflect"
 
 func walk(x interface{}, fn func(input string)){
+  // ValueOf returns a value of a given variable
+  // so we can inspect a value
   val := reflect.ValueOf(x)
+  // - and it's fields
   field := val.Field(0)
+
   fn(field.String())
 }
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,30 +1,58 @@
 package main
 
-import(
-  "testing"
+import (
+	"reflect"
+	"testing"
 )
 
 func TestWalk(t *testing.T){
-  t.Run("testing Walk()", func (t *testing.T){
+  // t.Run("testing Walk()", func (t *testing.T){
 
-    expected := "dgt"
-    var got []string
+  //   expected := "dgt"
+  //   var got []string
 
-    // creating anonymous struct
-    x := struct{
-      Name string
-    }{expected}
+  //   // creating anonymous struct
+  //   x := struct{
+  //     Name string
+  //   }{expected}
 
-    // walk with spy
-    walk(x, func(input string){
-      got = append(got, input)
+  //   // walk with spy
+  //   walk(x, func(input string){
+  //     got = append(got, input)
+  //   })
+  //   
+  //   if len(got) != 1{
+  //     t.Errorf("wrong number of function calls\ngot -> %d\nwant -> %d", len(got), 1)
+  //   }
+  //   if got[0] != expected{
+  //     t.Errorf("\ngot -> %q\nwant -> %q", got[0], expected)
+  //   }
+  // })
+
+  cases := []struct{
+    Name          string
+    Input         interface{}
+    ExpectedCalls []string
+  }{
+    {
+      "struct with one string field",
+      struct{
+        Name string
+      }{"dgt"},
+      []string{"dgt"},
+    },
+  }
+
+  for _, test := range cases{
+    t.Run(test.Name, func(t *testing.T){
+      var got []string
+      walk(test.Input, func(input string){
+        got = append(got, input)
+      })
+
+      if !reflect.DeepEqual(got, test.ExpectedCalls){
+        t.Errorf("\ngot -> %v\nwant -> %v", got, test.ExpectedCalls)
+      }
     })
-    
-    if len(got) != 1{
-      t.Errorf("wrong number of function calls\ngot -> %d\nwant -> %d", len(got), 1)
-    }
-    if got[0] != expected{
-      t.Errorf("\ngot -> %q\nwant -> %q", got[0], expected)
-    }
-  })
+  }
 }
