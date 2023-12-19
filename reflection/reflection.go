9169764bffbca116026fb020ca18fe25f90bc219
dogrento <dogrento93@gmail.com>
2023-12-19 10:38:39 -0300

    refactoring

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,24 +5,36 @@ import "reflect"
 func walk(x interface{}, fn func(input string)){
   val := getValue(x)
 
-  if val.Kind() == reflect.Slice{
-    for i := 0; i < val.Len(); i++{
-      walk(val.Index(i).Interface(), fn)
-    }
-
-    return
+  switch val.Kind(){
+  case reflect.Slice:
+  for i := 0; i < val.Len(); i++{
+    walk(val.Index(i).Interface(), fn)
   }
-
+  case reflect.Struct:
   for i := 0; i < val.NumField(); i++{
-    field := val.Field(i)
-
-    switch field.Kind(){
-    case reflect.String:
-      fn(field.String())
-    case reflect.Struct:
-      walk(field.Interface(), fn)
-    }
+    walk(val.Field(i).Interface(), fn)
   }
+  case reflect.String:
+    fn(val.String())
+  }
+  // if val.Kind() == reflect.Slice{
+  //   for i := 0; i < val.Len(); i++{
+  //     walk(val.Index(i).Interface(), fn)
+  //   }
+
+  //   return
+  // }
+
+  // for i := 0; i < val.NumField(); i++{
+  //   field := val.Field(i)
+
+  //   switch field.Kind(){
+  //   case reflect.String:
+  //     fn(field.String())
+  //   case reflect.Struct:
+  //     walk(field.Interface(), fn)
+  //   }
+  // }
 }
 
 func getValue(x interface{}) reflect.Value{
