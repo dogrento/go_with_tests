f94948b6004495db32d8c0287e89321960c94cd1
dogrento <dogrento93@gmail.com>
2023-12-20 10:26:45 -0300

    new_test: running safely concurrently

--- sync/syncg_test.go
+++ sync/syncg_test.go
@@ -1,7 +1,8 @@
 package main
 
-import(
-  "testing"
+import (
+	"sync"
+	"testing"
 )
 
 func TestCounter(t * testing.T){
@@ -12,6 +13,29 @@ func TestCounter(t * testing.T){
     counter.Inc()
 
     assertCounter(t, counter, 3)
+  })
+  t.Run("runs safely concurrently", func(t *testing.T){
+    wantedCount := 1000
+    counter := Counter{}
+
+    // waitgroup waits for a collection of goroutines to finish
+    var wg sync.WaitGroup
+    // calls Add to set the number of goroutines to wait for
+    wg.Add(wantedCount)
+
+    for i := 0; i < wantedCount; i++{
+      go func(){
+        counter.Inc()
+        // each of goroutines runs and calls Done when finished
+        wg.Done()
+      }()
+    }
+
+    // Wait() can be used to block until all goroutines have finished
+    // we can be sure all of our goroutines have attempted to Inc the Counter
+    wg.Wait()
+
+    assertCounter(t, counter, wantedCount)
   })
 }
 
