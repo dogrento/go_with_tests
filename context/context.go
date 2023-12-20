17a3ba674f14c95a19a53c57d90453c207d02f46
dogrento <dogrento93@gmail.com>
2023-12-20 18:46:09 -0300

    fixing code

--- context/context.go
+++ context/context.go
@@ -12,7 +12,23 @@ type Store interface{
 
 func Server(store Store) http.HandlerFunc{
   return func(w http.ResponseWriter, r *http.Request){
-    store.Cancel()
-    fmt.Fprint(w, store.Fetch())
+    ctx := r.Context()
+
+    data := make(chan string, 1)
+
+    go func(){
+      // Fetch() will write the result into a new chan 
+      data <- store.Fetch()
+    }()
+
+    // Using select to effectivbely race to the two asynchronous processes and then 
+    // either write a response or cancel.
+    select{
+    case d := <-data:
+      fmt.Fprint(w, d)
+    //Done() returns a chan which gets sent a signal when the context is done or cancelled.
+    case <-ctx.Done():
+      store.Cancel()
+    }
   }
 }
