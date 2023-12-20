c9efaece82fbe5e765e65412a4473a60d1f45623
dogrento <dogrento93@gmail.com>
2023-12-20 10:39:30 -0300

    fixing code: implementing mutex

--- sync/sync.go
+++ sync/sync.go
@@ -1,10 +1,17 @@
 package main
 
+import "sync"
+
 type Counter struct{
+  mu    sync.Mutex
   value int
 }
 
 func(c *Counter) Inc(){
+  // Any goroutine calling this func, will acquire the lock on Counter if they're first
+  // all the other goroutines will have to wait for it to be Unlocked before getting access
+  c.mu.Lock()
+  defer c.mu.Unlock()
   c.value++
 }
 func(c *Counter) Value() int{
