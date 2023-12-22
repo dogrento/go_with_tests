a2772265754c9892da0943e5c7bfb21802fee658
dogrento <dogrento93@gmail.com>
2023-12-22 06:50:32 -0300

    fixing code logic

--- property_based_tests/besed.go
+++ property_based_tests/besed.go
@@ -1,8 +1,15 @@
 package main
 
+import(
+  "strings"
+)
+
 func ConvertToRoman(value int) string{
-  if value == 2{
-    return "II"
+  var result strings.Builder
+
+  for i := 0; i < value; i++{
+    result.WriteString("I")
   }
-  return "I"
+
+  return result.String()
 }
