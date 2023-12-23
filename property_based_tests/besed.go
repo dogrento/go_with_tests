6b89694f1100bf7eac68cb99f22c1c3cbf80ad0f
dogrento <dogrento93@gmail.com>
2023-12-23 07:09:24 -0300

    fixing code

--- property_based_tests/besed.go
+++ property_based_tests/besed.go
@@ -5,13 +5,13 @@ import(
 )
 
 type RomanNumeral struct{
-  Value  int
+  Value  uint16
   Symbol string
 }
 
 type RomanNumerals []RomanNumeral
 
-func(r RomanNumerals) ValueOf(symbols ...byte) int{
+func(r RomanNumerals) ValueOf(symbols ...byte) uint16{
   symbol := string(symbols)
   for _, s := range r{
     if s.Symbol == symbol{
@@ -19,6 +19,16 @@ func(r RomanNumerals) ValueOf(symbols ...byte) int{
     }
   }
   return 0
+}
+
+func(r RomanNumerals) Exists(symbols ...byte) bool{
+  symbol := string(symbols)
+  for _, s := range r{
+    if s.Symbol == symbol{
+      return true
+    }
+  }
+  return false
 }
 
 var allRomanNumerals = RomanNumerals{
@@ -37,7 +47,27 @@ var allRomanNumerals = RomanNumerals{
   {1, "I"},
 }
 
-func ConvertToRoman(value int) string{
+type windowedRoman string
+func(w windowedRoman) Symbols() (symbols [][]byte){
+  for i := 0; i < len(w); i++{
+    symbol := w[1]
+    notAtEnd := i+1 < len(w)
+
+    if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]){
+      symbols = append(symbols, []byte{symbol, w[i+1]})
+      i++
+    }else{
+      symbols = append(symbols, []byte{symbol})
+    }
+  }
+  return
+}
+
+func isSubtractive(symbol uint8) bool{
+  return symbol == 'I' || symbol == 'X' || symbol == 'C'
+}
+
+func ConvertToRoman(value uint16) string{
 
   // Builder is used to efficiently build a string using Write methods
   // - It minimizes memory copying
@@ -53,28 +83,14 @@ func ConvertToRoman(value int) string{
   return result.String()
 }
 
-func ConvertToArabic(roman string) int{
-  total := 0
-
-  for i := 0; i < len(roman); i++{
-    symbol := roman[i]
-
-    if couldBeSubtractive(i, symbol, roman){
-      if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0{
-        total += value
-        i++
-      }else{
-        total += allRomanNumerals.ValueOf(symbol)
-      }
-    }else{
-      total += allRomanNumerals.ValueOf(symbol)
-    }
+func ConvertToArabic(roman string) (total uint16){
+  for _, symbols := range windowedRoman(roman).Symbols(){
+    total += allRomanNumerals.ValueOf(symbols...)
   }
-
-  return total
+  return 
 }
 
-func couldBeSubtractive(index int, symbol uint8, roman string) bool{
-  isSubtractiveSymbol := symbol == 'I' || symbol == 'X' || symbol == 'C'
-  return index+1 < len(roman) && isSubtractiveSymbol
-}
+// func couldBeSubtractive(index uint16, symbol uint16, roman string) bool{
+//   isSubtractiveSymbol := symbol == 'I' || symbol == 'X' || symbol == 'C'
+//   return index+1 < len(roman) && isSubtractiveSymbol
+// }
