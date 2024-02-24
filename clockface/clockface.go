adee389cebb19ec59e0ed06ced0d648b5253eda3
dogrento <dogrento93@gmail.com>
2024-02-24 20:50:29 -0300

    refactoring

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -1,8 +1,8 @@
 package clockface
 
 import(
-  // "fmt"
   "time"
+  "math"
 )
 
 // Point represents a 2d cartesian coordinate
@@ -11,8 +11,21 @@ type Point struct{
   Y float64
 }
 
-func SecondHand(t time.Time) Point{
-  // fmt.Printf("t: %v", t)
-  return Point{}
+func secondHandPoint(t time.Time) Point{
+  angle := secondsInRadians(t)
+  x:= math.Sin(angle)
+  y := math.Cos(angle)
+  return Point{x, y}
 }
 
+func SecondHand(t time.Time) Point{
+  p := secondHandPoint(t)
+  p = Point{p.X * 90, p.Y * 90}  // scale
+  p = Point{p.X, -p.Y}           // flip
+  p = Point{p.X + 150, p.Y + 150}// translate
+  return p
+}
+
+func secondsInRadians(t time.Time) float64{
+  return (math.Pi / (30 / (float64(t.Second()))))
+}
--- clockface/clockface_test.go
+++ clockface/clockface_test.go
@@ -76,32 +76,3 @@ func testName(t time.Time) string{
   return t.Format("15:04:05")
 }
 
-func TestSecondHandAtMidnight(t *testing.T){
-  tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-  want := clockface.Point{
-    X: 150,
-    Y: 150 - 90,
-  }
-
-  got := clockface.SecondHand(tm)
-
-  if got != want{
-    t.Errorf("got %v, wanted %v", got, want)
-  }
-}
-
-func TestSecondHandAt30Sec(t *testing.T){
-  tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-  want := clockface.Point{
-    X: 150,
-    Y: 150 + 90,
-  }
-
-  got := clockface.SecondHand(tm)
-
-  if got != want{
-    t.Errorf("got %v, wanted %v", got, want)
-  }
-}
