fc43443330762b3c9904501bd920c0822296e75b
dogrento <dogrento93@gmail.com>
2024-02-24 16:40:32 -0300

    creating and writing clockface unit test and its code

---
+++ clockface/clockface.go
@@ -0,0 +1,18 @@
+package clockface
+
+import(
+  // "fmt"
+  "time"
+)
+
+// Point represents a 2d cartesian coordinate
+type Point struct{
+  X float64
+  Y float64
+}
+
+func SecondHand(t time.Time) Point{
+  // fmt.Printf("t: %v", t)
+  return Point{}
+}
+
