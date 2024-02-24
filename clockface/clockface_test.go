94bc53c4cb72b05bd68b5de3869f461865a2e5f9
dogrento <dogrento93@gmail.com>
2024-02-24 17:16:31 -0300

    working with radians

--- clockface/clockface_test.go
+++ clockface/clockface_test.go
@@ -4,7 +4,22 @@ import(
   "go_with_tests/clockface"
   "testing"
   "time"
+  "math"
 )
+
+func TestSecondsInRadians(t *testing.T){
+  thirtySec := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+  want := math.Pi
+  got := secondsInRadians(thirtySec)
+
+  if got != want{
+    t.Errorf("got %v, wanted %v", got, want)
+  }
+}
+
+func secondsInRadians(t time.Time) float64{
+  return math.Pi
+}
 
 func TestSecondHandAtMidnight(t *testing.T){
   tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
@@ -12,6 +27,21 @@ func TestSecondHandAtMidnight(t *testing.T){
   want := clockface.Point{
     X: 150,
     Y: 150 - 90,
+  }
+
+  got := clockface.SecondHand(tm)
+
+  if got != want{
+    t.Errorf("got %v, wanted %v", got, want)
+  }
+}
+
+func TestSecondHandAt30Sec(t *testing.T){
+  tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+  want := clockface.Point{
+    X: 150,
+    Y: 150 + 90,
   }
 
   got := clockface.SecondHand(tm)
