c62f14777d36287b79a14e7a6453147f4d5317cc
dogrento <dogrento93@gmail.com>
2024-02-24 22:51:21 -0300

    creating svgwriter.go

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -18,14 +18,8 @@ func secondHandPoint(t time.Time) Point{
   return Point{x, y}
 }
 
-func SecondHand(t time.Time) Point{
-  p := secondHandPoint(t)
-  p = Point{p.X * 90, p.Y * 90}  // scale
-  p = Point{p.X, -p.Y}           // flip
-  p = Point{p.X + 150, p.Y + 150}// translate
-  return p
-}
-
 func secondsInRadians(t time.Time) float64{
   return (math.Pi / (30 / (float64(t.Second()))))
 }
+
+
