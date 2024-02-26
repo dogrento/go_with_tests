9a67593a5f1d886f406d2e73be2cc6d4ab25fbb8
dogrento <dogrento93@gmail.com>
2024-02-26 19:32:54 -0300

    creating of hour test

--- clockface/clockface_test.go
+++ clockface/clockface_test.go
@@ -32,6 +32,10 @@ func secondsInRadians(t time.Time) float64{
   return (math.Pi / (30 / (float64(t.Second()))))
 }
 
+func secondHandPoint(t time.Time) clockface.Point{
+  return angleToPoint(secondsInRadians(t))
+}
+
 func TestSecondHandPoint(t *testing.T){
   cases := []struct {
     time time.Time
@@ -96,6 +100,48 @@ func minutesInRadians(t time.Time) float64{
   return (secondsInRadians(t) / 60 + (math.Pi / (30 / float64(t.Minute())))) 
 }
 
+func TestHourInRadians(t *testing.T){
+  cases := []struct{
+    time time.Time
+    angle float64
+  }{
+    {simpleTime(6, 0, 0), math.Pi},
+    {simpleTime(0, 0, 0), 0},
+    {simpleTime(21, 0, 0), math.Pi * 1.5},
+  }
+
+  for _, c := range cases{
+    t.Run(testName(c.time), func(t *testing.T){
+      got := hoursInRadians(c.time)
+      if got != c.angle{
+        t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+      }
+    })
+  }
+}
+
+func hoursInRadians(t time.Time) float64{
+  return (math.Pi / (6 / float64(t.Hour())))
+}
+
+// func TestHourHandPoint(t *testing.T){
+//   cases := []struct {
+//     time time.Time
+//     point clockface.Point
+//   }{
+//     {simpleTime(30, 0, 0), clockface.Point{0, -1}},
+//   }
+
+//   for _, c := range cases{
+//     t.Run(testName(c.time), func(t *testing.T){
+//       got := minuteHandPoint(c.time)
+//       if !roughlyEqualPoint(got, c.point){
+//         t.Fatalf("got %v radians, wanted %v", got, c.point)
+//       }
+//     })
+//   }
+// }
+
 func angleToPoint(angle float64) clockface.Point{
   x := math.Sin(angle)
   y := math.Cos(angle)
@@ -111,11 +157,6 @@ func roughlyEqualFloat64(a, b float64) bool{
 func roughlyEqualPoint(a, b clockface.Point) bool{
   return roughlyEqualFloat64(a.X, b.X) && roughlyEqualFloat64(a.Y, b.Y)
 }
-
-func secondHandPoint(t time.Time) clockface.Point{
-  return angleToPoint(secondsInRadians(t))
-}
-
 
 func simpleTime(hours, minutes, seconds int) time.Time{
   return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
